227400254f8c2d9f470fbc7bc9f4961377f43e00
Mohamed Illiyas <120028367+Illiyas124@users.noreply.github.com>
2024-11-30 14:53:08 +0000

    environmental variable DB connacted

--- 20_11_2024/dbconf.php
+++ 20_11_2024/dbconf.php
@@ -7,11 +7,12 @@ $dbHost = getenv('DB_HOST');
 $dbName = getenv('DB_NAME');
 $dbUser = getenv('DB_USER');
 $dbPass = getenv('DB_PASS');
-
+//mysql -h $DB_HOST -u root -p and password is : mariadb
 
 //connect with database
 try{
-$connect=mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DNAME);
+//$connect=mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DNAME);
+$connect=mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
     if(!$connect){
         die("connection failed".mysqli_connect_error());
     }
