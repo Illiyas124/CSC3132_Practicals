5851bbc5ddb784db56ee829e6e68de3023f5b8dc
Mohamed Illiyas <120028367+Illiyas124@users.noreply.github.com>
2024-12-09 11:08:44 +0000

    added gettable.php

---
+++ 4.12.2024/gettable.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<?php
+//get the db connection file
+
+require_once 'dbconf.php';
+require_once 'myfunc.php';
+
+PrintTable ("student",$connect);
+
+
+?>
+
+<form action = "<?php echo $_SERVER['PHP_SELF']; ?>" method = "GET">
+	<table >
+		<tr >
+			<td align=right>Student Name : </td>
+			<td><input type="text" name="name"/></td>
+		</tr>
+		<tr>
+			<td><input type="submit" value="Search"></td>
+		</tr>
+
+	</table>
+
+</form>
+<?php
+
+if (isset($_GET['name']) && $_GET['name'] != '') {
+	Searchemployee($_GET['name'],$connect);
+}
+
+
+?>
+</body>
+</html>
